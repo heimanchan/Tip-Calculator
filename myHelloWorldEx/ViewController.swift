7b757d0a68b5e6edaa07d1c5af749e9ec6c2c8aa
Heiman Chan <heimanchan0@gmail.com>
2019-06-12 16:43:33 -0700

    Initial Commit

---
+++ myHelloWorldEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myHelloWorldEx
+//
+//  Created by Heiman Chan on 6/12/19.
+//  Copyright © 2019 Heiman Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
