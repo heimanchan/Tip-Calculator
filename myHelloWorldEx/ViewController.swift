2f6ca8fe373606d2651a4b6b194c5691ffe3134d
Heiman Chan <heimanchan0@gmail.com>
2019-06-13 00:49:23 -0700

    Add keyboard event listeners functions

--- myHelloWorldEx/ViewController.swift
+++ myHelloWorldEx/ViewController.swift
@@ -8,18 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
   @IBOutlet weak var tenPercentLabel : UILabel!
   @IBOutlet weak var fifteenPercentLabel : UILabel!
   @IBOutlet weak var twentyPercentLabel : UILabel!
   @IBOutlet weak var subtotalTextField: UITextField!
   
-  @IBAction func calculateButtonPressed(_ sender: Any) {
-    print("test")
-  }
-  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    subtotalTextField.delegate = self
+    
+    // Listen for keyboard events
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillChange(notification:)),
+      name: UIResponder.keyboardWillShowNotification,
+      object: nil
+    )
+    
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillChange(notification:)),
+      name: UIResponder.keyboardWillHideNotification,
+      object: nil
+    )
+    
+    NotificationCenter.default.addObserver(self,
+      selector: #selector(keyboardWillChange(notification:)),
+      name: UIResponder.keyboardWillChangeFrameNotification,
+      object: nil
+    )
+  }
+  
+  deinit {
+    NotificationCenter.default.removeObserver(
+      self,
+      name: UIResponder.keyboardWillShowNotification,
+      object: nil
+    )
+    
+    NotificationCenter.default.removeObserver(
+      self,
+      name: UIResponder.keyboardWillHideNotification,
+      object: nil
+    )
+    
+    NotificationCenter.default.removeObserver(
+      self,
+      name: UIResponder.keyboardWillChangeFrameNotification,
+      object: nil
+    )
+  }
+  
+  // Actions
+  @IBAction func calculateButtonPressed(_ sender: Any) {
+    print("Calculate Button Clicked")
+    subtotalTextField.text = "Free"
+  }
+  
+  // Functions
+  // UIDelegate funcs
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    print("Pressed return")
+    hideKeyboard()
+    return true
+  }
+  
+  func hideKeyboard() {
+    subtotalTextField.resignFirstResponder()
+  }
+  
+  @objc func keyboardWillChange(notification: Notification) {
+    print("Keyboard will show: \(notification.name.rawValue)")
   }
 
 }
